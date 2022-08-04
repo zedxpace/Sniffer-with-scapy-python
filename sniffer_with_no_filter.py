f09c6c40917f7328504ed361bcef9617a13f91e1
zedxpace <52217313+zedxpace@users.noreply.github.com>
2022-08-04 17:36:12 +0530

    Delete sniffer_with_no_filter.py

--- sniffer_with_no_filter.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import *
-
-#callback function that will recieve each sniffed packet
-def packet_callback(packet):
-    print(packet.show())
-
-#fire up the sniffer on all interfaces with no filtering
-sniff(prn=packet_callback ,count=1)
